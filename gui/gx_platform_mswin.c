b291bb42c1a31cd3867c41b1de0ce04d6ead43c2
Thorsten Maerz <info@netztorte.de>
2023-01-17 14:07:20 +0100

    MSWin: correct name passed to UnregisterClass()

--- gui/gx_platform_mswin.c
+++ gui/gx_platform_mswin.c
@@ -71,7 +71,7 @@ void gx_gui_register_controller_message(gx_MicroAmpUI *ui) {
 void gx_gui_destroy_main_window(gx_MicroAmpUI *ui) {
 	DestroyWindow(ui->win);
 	// safe to use: doesnt unregister if there are still windows of this class
-	UnregisterClass(TEXT("gx_DrawSurfaceClass"), NULL);
+	UnregisterClass(TEXT("gx_MicroAmpUIClass"), NULL);
 }
 
 void gx_gui_resize_surface(gx_MicroAmpUI *ui) {
